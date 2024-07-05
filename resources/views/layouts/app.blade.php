a13b43fc056328b8f67425e90ef9e5b49ed8a9b5
alessandro-barate <alebarat@gmail.com>
2024-07-05 15:45:20 +0100

    [UPD] Extends and yield added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Boolando</title>
+</head>
+
+<body>
+
+    {{-- Including Header --}}
+    @include('shared.header')
+    {{-- END Including Header --}}
+
+    <main>
+        @yield('main')
+    </main>
+
+    {{-- Including Footer --}}
+    @include('shared.footer')
+    {{-- END Including Footer --}}
+
+</body>
+
+</html>
